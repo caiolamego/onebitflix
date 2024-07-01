28111fab0b29a3ca712dff771c389d37372f38d1
caiolamego <caio.lamego04@gmail.com>
2024-07-01 19:10:49 -0300

    Upload das capas dos cursos

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,7 +1,7 @@
 import { ResourceWithOptions } from "adminjs";
 import { Category, Course } from "../../models";
 import { categoryResourceOptions } from "./category";
-import { courseResourceOptions } from "./course";
+import { courseResourceFeatures, courseResourceOptions } from "./course";
 import { Episode } from "../../models/episode";
 import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
 
@@ -12,7 +12,8 @@ export const adminJsResources: ResourceWithOptions[] = [
     },
     {
         resource: Course,
-        options: courseResourceOptions
+        options: courseResourceOptions,
+        features: courseResourceFeatures
     }, 
     {
         resource: Episode,
