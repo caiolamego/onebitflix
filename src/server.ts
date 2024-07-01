6f1a1deb704016600ea378c31ba1c23208308ac0
caiolamego <caio.lamego04@gmail.com>
2024-07-01 11:39:55 -0300

    inciando projeto

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+//import { Express } from "express";
+import express from "express";
+
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`)
+})
