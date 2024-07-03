42fc560d887dd6efcd9b0aea8b17e7e263c5a730
caiolamego <caio.lamego04@gmail.com>
2024-07-03 12:25:08 -0300

    Obtendo cursos de uma categoria

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -18,5 +18,22 @@ export const categoryService = {
             total: count
         }
 
+    },
+
+    findByIdWithCourses: async (id: string) => {
+        const categoryWithCourses = await Category.findByPk(id, {
+            attributes: ['id', 'name'],
+            include: {
+                association: 'courses',
+                attributes: [
+                    'id',
+                    'name',
+                    'synopsis',
+                    ['thumbnail_url', 'thumbnailUrl']
+                ]
+            }
+        })
+
+        return categoryWithCourses
     }
 }
