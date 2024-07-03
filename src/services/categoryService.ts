4fda14b193a959a9a0ffaa5756bfc198f77fbfdb
caiolamego <caio.lamego04@gmail.com>
2024-07-03 12:11:55 -0300

    incluindo paginação de categorias

---
+++ src/services/categoryService.ts
@@ -0,0 +1,22 @@
+import { Category } from "../models"
+
+
+export const categoryService = {
+    findAllPaginated: async (page: number, perPage: number) => {
+        const offset = (page - 1) * perPage
+
+        const { count, rows } = await Category.findAndCountAll({
+            order: [['position', 'ASC']],
+            limit: perPage,
+            offset
+        })
+
+        return {
+            categories: rows,
+            page: page,
+            perPage: perPage,
+            total: count
+        }
+
+    }
+}
